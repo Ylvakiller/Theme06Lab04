b8417defa5af3cf8ba4361a25e9e75a2a14841f4
Ylvakiller <rem.geuze@gmail.com>
2015-01-04 19:32:32 +0100

    Started lab4

---
+++ Project1/project1/Person.java
@@ -0,0 +1,28 @@
+package project1;
+
+public class Person {
+	private String name;
+	public Person(){
+		name = "No Name Yet";
+	}
+	public Person(String initialName)
+    {
+        name = initialName;
+    }
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void writeOutput()
+    {
+        System.out.println("name: " + name);
+    }
+    public boolean hasSameName(Person otherPerson)
+    {
+        return this.name.equalsIgnoreCase(otherPerson.name);
+    }
+}
