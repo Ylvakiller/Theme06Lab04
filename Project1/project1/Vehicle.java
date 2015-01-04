92fb6143eb6f2c0587d05f66db81778211ad1c6d
Ylvakiller <rem.geuze@gmail.com>
2015-01-04 20:24:37 +0100

    Started the vehicle class

---
+++ Project1/project1/Vehicle.java
@@ -0,0 +1,18 @@
+package project1;
+
+public class Vehicle {
+	/**
+	 * The manufacturers name
+	 */
+	private String manufacturersName;
+	/**
+	 * The number of cylinders that this vehicle has
+	 */
+	private int numberOfCilinders;
+	
+	/**
+	 * This is the owner of this vehicle
+	 */
+	private Person owner;
+	
+}
