9c1e9dbb665b9e50d96857a68de6bbda157ba527
Ylvakiller <rem.geuze@gmail.com>
2015-01-31 13:09:33 +0100

    Changed the triangle to only have size
    This to make the drawing of the size easier

--- Project2/project2/RightTriangle.java
+++ Project2/project2/RightTriangle.java
@@ -4,37 +4,25 @@ public class RightTriangle extends ShapeBase {
 	/**
 	 * The height of the triangle
 	 */
-	private int height;
+	private int size;
 	/**
-	 * The width of the triangle
-	 */
-	private int width;
-	
-	/**
-	 * Basic constructor, will set the height and width to 1 and the offset to 0
+	 * Basic constructor, will set the size to 1 and the offset to 0
 	 */
 	public RightTriangle() {
-		height = 1;
-		width =1;
+		size = 1;
 		offset =0;
 	}
 	/**
-	 * Will set the height and the width to the input,
+	 * Will set the size to the input,
 	 * Will set the offset to 0
 	 * Will throw a NumberFormatException if the given input is of incorrect form
-	 * @param height the height, must be a positive integer (not including 0)
-	 * @param width the width, must be a positive integer (not including 0)
+	 * @param size the height, must be a positive integer (not including 0)
 	 */
-	public RightTriangle(int height, int width){
-		if (height <=0){
-			throw new NumberFormatException("The height entered, " + height + " is below or equal to 0, this is an invalid input");
+	public RightTriangle(int size){
+		if (size <=0){
+			throw new NumberFormatException("The height entered, " + size + " is below or equal to 0, this is an invalid input");
 		}else{
-			this.height = height;
-		}
-		if (width<=0){
-			throw new NumberFormatException("The width entered, " + width + " is below or equal to 0, this is an invalid input");
-		}else{
-			this.width = width;
+			this.size = size;
 		}
 		offset = 0;
 	}
@@ -43,17 +31,27 @@ public class RightTriangle extends ShapeBase {
 	 */
 	@Override
 	public void drawHere() {
-		// TODO Auto-generated method stub
+		int i = 0;
+		while (i<offset){
+			System.out.println("");
+		}
+		i = 0;
+		int y = 0;
+		while (i<size){
+			System.out.print("|");
+		}
 
 	}
 
 	/**
 	 * This method will calculate the area and return that to the user
+	 * Since this class only has a size parameter (instead of width and height) we can simply return the size,
+	 * This since the formula for the area of a triangle is Height*width/2
 	 * @return a double value calculated for the area of 
 	 */
 	@Override
 	public double calcArea() {
-		return (double)(height*width)/2;
+		return size;
 	}
 	/**
 	 * This method will calculate and return the circumference
@@ -61,7 +59,7 @@ public class RightTriangle extends ShapeBase {
 	 */
 	@Override
 	public double calcCircumference() {
-		double extraSide = Math.pow(height,2)+Math.pow(width,2);
+		double extraSide = Math.pow(size,2)+Math.pow(size,2);
 		return Math.sqrt(extraSide);
 	}
 
