d95395b616c8a05aca70d8a5d749c86770f32c14
Ylvakiller <rem.geuze@gmail.com>
2015-02-02 20:57:47 +0100

    Started rectangle

---
+++ Project2/project2/Rectangle.java
@@ -0,0 +1,31 @@
+package project2;
+
+public class Rectangle extends ShapeBase {
+
+	int width;
+	
+	int height;
+	
+	public Rectangle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void drawHere() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double calcArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calcCircumference() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
