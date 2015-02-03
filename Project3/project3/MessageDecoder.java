a33b7ae03bd293a9b0057b8e593c52c5083600b6
Ylvakiller <rem.geuze@gmail.com>
2015-02-03 12:32:23 +0100

    Fixed a typo in the javadoc

--- Project3/project3/MessageDecoder.java
+++ Project3/project3/MessageDecoder.java
@@ -2,7 +2,7 @@ package project3;
 
 public interface MessageDecoder {
 	/**
-	 * This method should be able to decode the given input utilising the key
+	 * This method should be able to decode the given input utilizing the key
 	 * @param input the string to decode
 	 * @param key the key used in the encoding process
 	 * @return the resulting decoded string
